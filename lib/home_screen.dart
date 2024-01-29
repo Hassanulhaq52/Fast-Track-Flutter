37cd6c946643769520afbed60b795961c7fd2bee
Hassanulhaq52 <Hassanulhaq52@gmail.com>
2024-01-29 12:11:17 +0500

    Bottom Nav Bar & Tab Bar.

---
+++ lib/home_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
+          ),
+          // bottomNavigationBar: BottomNavigationBar(
+          //   items: [
+          //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          //     BottomNavigationBarItem(
+          //         icon: Icon(Icons.person), label: 'Person'),
+          //     BottomNavigationBarItem(
+          //         icon: Icon(Icons.search), label: 'Search'),
+          //   ],
+          // ),
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Icon(Icons.home),
+                Icon(Icons.person_off_rounded),
+                Icon(Icons.add),
+              ],
+            ),
+            title: Text('Platform Specific Widgets'),
+            backgroundColor: Colors.green,
+            centerTitle: true,
+            leading: Icon(Icons.arrow_back),
+            actions: [
+              CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Icon(Icons.person),
+              ),
+            ],
+          ),
+          body: TabBarView(
+            children: [
+              Center(child: Text('This is Home Screen')),
+              Center(child: Text('This is Person Screen')),
+
+              Center(child: Text('This is Add Screen')),
+              // Icon(Icons.add),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
