0b1080845a0550108e4b2b649e7cc70c9ea45369
hassanulhaq52 <hassanulhaq52@gmail.com>
2024-02-15 12:51:37 +0500

    Stream Builder Screen.

---
+++ lib/builders/stream_builder_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class StreamBuilderSCreen extends StatelessWidget {
+  const StreamBuilderSCreen({super.key});
+
+  Stream generateNumbers() async* {
+    int count = 0;
+
+    while (true) {
+      await Future.delayed(Duration(seconds: 5),);
+      yield count++;
+    }
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: generateNumbers(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('${snapshot.error}'),
+            );
+          } else if (snapshot.hasData) {
+           return Center(
+              child: Text('${snapshot.data}', style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold),),
+            );
+          } else {
+            return Text("Invalid Error");
+          }
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:platform_specific/builders/future_builder_screen.dart';
 import 'package:platform_specific/builders/splash_screen.dart';
+import 'package:platform_specific/builders/stream_builder_screen.dart';
 import 'package:platform_specific/styling_responsiveness/orientation_builder_screen.dart';
 
 void main() {
@@ -16,6 +17,6 @@ class MyApp extends StatelessWidget {
         title: 'Platform Specific Widgets',
         debugShowCheckedModeBanner: false,
         theme: ThemeData.dark(),
-        home: SplashScreen(),);
+        home: StreamBuilderSCreen(),);
   }
 }
